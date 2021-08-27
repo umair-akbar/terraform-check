b31585f2851ed38c5b8825121cdf761b4fab6f04
Umair Akbar <uakbar@deloitte.com>
2021-08-27 12:26:40 -0500

    Update resources.tf

--- infra/resources.tf
+++ infra/resources.tf
@@ -1,233 +1,74 @@
-terraform {
-    required_version = ">= 0.12.0"
-}
-
-provider "aws" {
-    region = "us-west-2"
-}
-
-resource "tls_private_key" "this" {
-    algorithm = "RSA"
-    rsa_bits = 4096
-}
-
-resource "aws_key_pair" "server-key" {
-    key_name = "server-deployer-key"
-    public_key = tls_private_key.this.public_key_openssh
-}
-
-variable "vpc_name" {
-    description = "name of the vpc"
-    default = "tf-new-example"
-}
-
-resource "aws_vpc" "new_vpc" {
-    cidr_block = "172.16.0.0/16"
-    enable_dns_hostnames = true
-
-    tags = {
-        Name = var.vpc_name
-    }
-}
-
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.ok_vpc.id
-}
-
-
-locals {
-    golang_user_data = <<EOF
-#!/bin/bash
-wget -O card-gen https://github.com/we45/Redis-Go-Service/releases/download/1.1/card-gen && chmod +x card-gen && mv card-gen /usr/bin
-card-gen 172.16.10.101 6379 &
-EOF
-
-}
-
-locals {
-    redis_user_data = <<EOF
-#!/bin/bash
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
-docker pull redis
-docker run -d -p 6379:6379 redis
-EOF
-}
-
-resource "aws_internet_gateway" "my_igw" {
-    vpc_id = aws_vpc.new_vpc.id
-
-    tags = {
-        Name = "IGW-Test-VPC"
-    }
-}
-
-resource "aws_route_table" "igw_router" {
-    vpc_id = aws_vpc.new_vpc.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.my_igw.id
-    }
-
-    tags = {
-        Name = "public-ingress-routed-gateway"
-    }
-}
-
-resource "aws_subnet" "new_sub" {
-    vpc_id = aws_vpc.new_vpc.id
-    cidr_block = "172.16.10.0/24"
-    availability_zone = "us-west-2a"
-    map_public_ip_on_launch = true
-
-    tags = {
-        Name = "tf-subnet-example"
-    }
-}
-
-resource "aws_route_table_association" "subnet_igw" {
-    subnet_id = aws_subnet.new_sub.id
-    route_table_id = aws_route_table.igw_router.id
-}
-
-resource "aws_security_group" "allow-ssh" {
-    name = "allow_ssh"
-    description = "Allow SSH inbound traffic from the internet"
-    vpc_id = aws_vpc.new_vpc.id
-
-    ingress {
-        description = "Allow SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        description = "Redis in VPC"
-        from_port = 6379
-        to_port = 6379
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-
-resource "aws_network_interface" "goo" {
-    subnet_id = aws_subnet.new_sub.id
-    private_ips = ["172.16.10.100"]
-    security_groups = [aws_security_group.allow-ssh.id]
-    tags = {
-        Name = "golang-network-interface"
-    }
-}
-
-resource "aws_network_interface" "redis" {
-    subnet_id = aws_subnet.new_sub.id
-    private_ips = ["172.16.10.101"]
-    security_groups = [aws_security_group.allow-ssh.id]
-    tags = {
-        Name = "redis-network-interface"
-    }
-}
-
-resource "aws_instance" "golang-app" {
-    ami = "ami-053bc2e89490c5ab7"
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.server-key.key_name
-    user_data_base64 = base64encode(local.golang_user_data)
-
-    tags = {
-        Name = "tf-example-ec2-instance"
-    }
-
-    network_interface {
-        network_interface_id = aws_network_interface.goo.id
-        device_index = 0
-    }
-
-    depends_on = ["aws_instance.redis-db"]
-}
-
-resource "aws_instance" "redis-db" {
-    ami = "ami-053bc2e89490c5ab7"
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.server-key.key_name
-    user_data_base64 = base64encode(local.redis_user_data)
-
-    tags = {
-        Name = "tf-example-ec2-instance"
-    }
-
-    network_interface {
-        network_interface_id = aws_network_interface.redis.id
-        device_index = 0
-    }
-}
-
-resource "local_file" "aws_key" {
-    content = tls_private_key.this.private_key_pem
-    filename = "test-key.pem"
-}
-
-output "private_dns" {
-    value = aws_instance.redis-db.private_ip
-    description = "This is the Private IP of the newly created redis instance"
-}
-
-output "public_redis_ip" {
-    value = aws_instance.redis-db.public_ip
-    description = "Public IP for the Redis Server"
-}
-
-output "private_golang_ip" {
-    value = aws_instance.golang-app.private_ip
-    description = "This is the Private IP of the newly created golang app server"
-}
-
-output "public_golang_ip" {
-    value = aws_instance.golang-app.public_ip
-    description = "Public IP for the golang app server"
-}
-
-resource "random_string" "bucket_name" {
-    length = 10
-    special = false
-    upper = false
-}
-
-resource "aws_s3_bucket" "bucket_good_1" {
-  bucket = "bucket_good"
-}
-
-resource "aws_s3_bucket_public_access_block" "access_good_1" {
-  bucket = aws_s3_bucket.bucket_good_1.id
-
-  block_public_acls   = true
-  block_public_policy = true
-}
-
-data "aws_caller_identity" "current" {}
-
-output "s3_bucket_url" {
-    value = "s3://${aws_s3_bucket.flow_log_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/vpcflowlogs/us-west-2/"
-    description = "s3 bucket url with the URL notation"
-}
-
-resource "aws_flow_log" "example" {
-    log_destination = aws_s3_bucket.flow_log_bucket.arn
-    log_destination_type = "s3"
-    traffic_type = "ALL"
-    vpc_id = aws_vpc.new_vpc.id
-}
+AWSTemplateFormatVersion: 2010-09-09
+Description: >-
+  This template creates an Amazon S3 bucket that will be used for storing S3 server access logs.
+Metadata:
+  AWS::CloudFormation::Interface:
+    ParameterGroups:
+      - Label:
+          default: S3 Server Access Logs Configuration
+        Parameters:
+          - S3AccessLogsBucketName
+    ParameterLabels:
+      S3AccessLogsBucketName:
+        default: S3 Server Access Logs Bucket Name
+Parameters:
+  S3AccessLogsBucketName:
+    AllowedPattern: '^$|^(?=^.{3,63}$)(?!.*[.-]{2})(?!.*[--]{2})(?!^(?:(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(?!$)|$)){4}$)(^(([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])$)'
+    ConstraintDescription:
+      S3 bucket name can include numbers, lowercase letters, uppercase letters, and hyphens (-). It cannot start or end with a hyphen (-).
+    Default: ''
+    Description:
+      (Optional) S3 Server Access Logs bucket name for where Amazon S3 should store server access log files. S3 bucket name can include numbers,
+      lowercase letters, uppercase letters, and hyphens (-). It cannot start or end with a hyphen (-). If empty, a new S3 bucket will be created as a
+      destination for S3 server access logs, it will follow the format, aws-s3-access-logs-<account>-<region>
+    Type: String
+Conditions:
+  S3AccessLogsBucketNameCondition: !Not [!Equals [!Ref S3AccessLogsBucketName, '']]
+Resources:
+  S3AccessLogsBucket:
+    Metadata:
+      cfn_nag:
+        rules_to_suppress:
+          - id: W35
+            reason:
+              Amazon S3 server access logs not enabled on this bucket, as this bucket is the target for storing all Amazon S3 server access logs.
+    Type: AWS::S3::Bucket
+    Properties:
+      BucketName: !If
+        - S3AccessLogsBucketNameCondition
+        - !Ref S3AccessLogsBucketName
+        - !Sub aws-s3-access-logs-${AWS::AccountId}-${AWS::Region}
+      AccessControl: LogDeliveryWrite
+      PublicAccessBlockConfiguration:
+        BlockPublicAcls: True
+        BlockPublicPolicy: True
+        IgnorePublicAcls: True
+        RestrictPublicBuckets: True
+      BucketEncryption:
+        ServerSideEncryptionConfiguration:
+          - ServerSideEncryptionByDefault:
+              SSEAlgorithm: AES256
+      VersioningConfiguration:
+        Status: Enabled
+  S3AccessLogsBucketPolicy:
+    Type: AWS::S3::BucketPolicy
+    Properties:
+      Bucket: !Ref S3AccessLogsBucket
+      PolicyDocument:
+        Statement:
+          - Sid: DenyNonSSLRequests
+            Effect: Deny
+            Action: s3:*
+            Resource:
+              - !Sub arn:${AWS::Partition}:s3:::${S3AccessLogsBucket}
+              - !Sub arn:${AWS::Partition}:s3:::${S3AccessLogsBucket}/*
+            Principal: '*'
+            Condition:
+              Bool:
+                aws:SecureTransport: false
+Outputs:
+  S3AccessLogsBucketName:
+    Description: S3 Server Access Logs Bucket Name
+    Value: !Ref S3AccessLogsBucket
+    Export:
+      Name: !Sub '${AWS::StackName}-S3AccessLogsBucketName'
